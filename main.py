e0648c144b6485c26b8f0401a7e5daf73f67d45e
ArVi <ars.v.p546@gmail.com>
2024-02-29 19:08:03 +0300

    Зашел

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 im = Image.new("RGB",(500,300),(219,193,27))
 draw=ImageDraw.Draw(im)
+#абоба
 draw.ellipse((100,100,150,200),fill="purple", outline='white')
 im.show()
